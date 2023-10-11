b7008cf2b2b4efb75fb731a72fb3b1c38d31c583
tungdevpro <tungdo21899@gmail.com>
2023-10-12 06:42:49 +0700

    feat: add getting_started page

---
+++ lib/presentation/getting_started/getting_started_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GettingStartedPage extends StatelessWidget {
+  const GettingStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

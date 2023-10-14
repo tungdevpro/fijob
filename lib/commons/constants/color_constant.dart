becf42bbeab2413f282cf0505856360d4936be10
tungdevpro <tungdo21899@gmail.com>
2023-10-14 10:02:05 +0700

    feat: add color resource

--- lib/commons/constants/color_constant.dart
+++ lib/commons/constants/color_constant.dart
@@ -4,5 +4,23 @@ import 'package:flutter/material.dart';
 abstract class ColorConstants {
   const ColorConstants._();
 
-  static const Color primary = Colors.blue;
+  static const Color primary = Color(0xFF00CC9A);
+  static const Color primary2 = Color(0xFF3FAC5C);
+  static const Color secondary = Color(0xFFFE970F);
+  static const Color alertSuccess = Color(0xFF00C566);
+  static const Color alertError = Color(0xFFFF4747);
+  static const Color alertWarning = Color(0xFFFACC15);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color line = Color(0xFFE3E7EC);
+  static const Color lineDark = Color(0xFF4A4A65);
+  static const Color gray10 = Color(0xFFFDFDFD);
+  static const Color gray20 = Color(0xFFECF1F6);
+  static const Color gray30 = Color(0xFFE3E9ED);
+  static const Color gray40 = Color(0xFFD1D8DD);
+  static const Color gray50 = Color(0xFFBFC6CC);
+  static const Color gray60 = Color(0xFF9CA4AB);
+  static const Color gray70 = Color(0xFF78828A);
+  static const Color gray80 = Color(0xFF66707A);
+  static const Color gray90 = Color(0xFF434E58);
+  static const Color gray100 = Color(0xFF171725);
 }

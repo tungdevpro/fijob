dd881917f224f35f14039ec37aca65399f956af0
tungdevpro <tungdo21899@gmail.com>
2023-10-15 08:11:19 +0700

    feat: add func toJsonMethod

--- packages/yh_basic/lib/types.dart
+++ packages/yh_basic/lib/types.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 enum LoginOption { none, required }
 
 typedef AsyncCallbackFunc = Future<void> Function();
 typedef JsonMap = Map<String, dynamic>;
+
+String toJsonMethod(Map<String, dynamic> obj) => jsonEncode(obj);
+
+Map<String, dynamic> fromJsonMethod(String json) => jsonDecode(json) as Map<String, dynamic>;

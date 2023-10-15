4f6653e376c8a9e3b2cba06e516d30a996c22fd2
tungdevpro <tungdo21899@gmail.com>
2023-10-15 08:03:47 +0700

    feat: add app style dimen & typography

---
+++ lib/commons/constants/app_dimens.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class AppDimens {
+  const AppDimens._();
+
+  static const double padding = 12.0;
+  static const double radius = 24.0;
+}
---
+++ lib/commons/constants/app_typography.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class AppTypography {
+  const AppTypography._();
+
+  static const double extraSmallFs = 10.0;
+  static const double smallFs = 12.0;
+  static const double mediumFs = 14.0;
+  static const double largeFs = 16.0;
+  static const double extraLargeFs = 18.0;
+}

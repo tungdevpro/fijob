f6676ecc4894405082befba1c4b1be008c052e9f
tungdevpro <tungdo21899@gmail.com>
2023-10-15 08:09:36 +0700

    feat: add route getting_started

--- lib/core/navigator/routes.dart
+++ lib/core/navigator/routes.dart
@@ -1,20 +1,56 @@
 import 'package:fijob/core/navigator/route_path.dart';
+import 'package:fijob/core/navigator/screens/error_screen.dart';
+import 'package:fijob/presentation/getting_started/getting_started_page.dart';
+import 'package:fijob/presentation/home/components/detail_home_page.dart';
 import 'package:fijob/presentation/initial/initial_page.dart';
 import 'package:fijob/presentation/login/login_page.dart';
+import 'package:fijob/presentation/register/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../presentation/home/home_page.dart';
 
 @immutable
 abstract class Routes {
   const Routes._();
 
   static GoRouter router = GoRouter(
+    errorBuilder: (_, __) => const ErrorScreen(),
     routes: [
       GoRoute(path: RoutePath.initial, builder: (context, state) => const InitialPage()),
-      GoRoute(path: RoutePath.home, builder: (context, state) => const InitialPage()),
-      GoRoute(path: RoutePath.gettingStarted, builder: (context, state) => const InitialPage()),
+      GoRoute(
+        path: RoutePath.home,
+        builder: (context, state) => const HomePage(),
+        routes: [
+          GoRoute(
+            path: ":id",
+            pageBuilder: (context, state) {
+              var id = state.pathParameters["id"];
+              return CustomSlideTransition(child: DetailHomePage(id: id!));
+            },
+          ),
+        ],
+      ),
+      GoRoute(path: RoutePath.gettingStarted, builder: (context, state) => const GettingStartedPage()),
       GoRoute(path: RoutePath.login, builder: (context, state) => const LoginPage()),
-      GoRoute(path: RoutePath.register, builder: (context, state) => const InitialPage())
+      GoRoute(path: RoutePath.register, builder: (context, state) => const RegisterPage())
     ],
   );
+}
+
+class CustomSlideTransition extends CustomTransitionPage<void> {
+  CustomSlideTransition({super.key, required super.child})
+      : super(
+          transitionDuration: const Duration(milliseconds: 300),
+          transitionsBuilder: (_, animation, __, child) {
+            return SlideTransition(
+              position: animation.drive(
+                Tween(begin: const Offset(1.5, 0), end: Offset.zero).chain(
+                  CurveTween(curve: Curves.easeInOut),
+                ),
+              ),
+              child: child,
+            );
+          },
+        );
 }

8bfbb63ca709271b8f5b43437525f00bcc0c0140
tungdevpro <tungdo21899@gmail.com>
2023-10-17 11:13:32 +0700

    feat(asset): add image no connection file

--- lib/commons/constants/asset/image_resource.dart
+++ lib/commons/constants/asset/image_resource.dart
@@ -5,4 +5,5 @@ static const String imgGettingStarted2 = 'assets/images/getting-started-2.png';
 	static const String imgGettingStarted1 = 'assets/images/getting-started-1.png';
 	static const String imgDsSt = 'assets/images/.DS_Store';
 	static const String imgSplash = 'assets/images/splash.png';
+	static const String imgNoConnection = 'assets/images/No-Connection.png';
 }

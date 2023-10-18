0b5daa018c9c2101d65816807673498decaa39af
tungdevpro <tungdo21899@gmail.com>
2023-10-18 22:42:32 +0700

    feat: add getting started

--- lib/commons/constants/asset/image_resource.dart
+++ lib/commons/constants/asset/image_resource.dart
@@ -2,6 +2,7 @@ abstract class ImageResource {
 	const ImageResource._();
 
 static const String imgGettingStarted2 = 'assets/images/getting-started-2.png';
+	static const String imgGettingStarted3 = 'assets/images/getting-started-3.png';
 	static const String imgGettingStarted1 = 'assets/images/getting-started-1.png';
 	static const String imgDsSt = 'assets/images/.DS_Store';
 	static const String imgSplash = 'assets/images/splash.png';

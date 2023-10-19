36459f63f5ece4f82efe04fa0adc66161f83b4e2
tungdevpro <tungdo21899@gmail.com>
2023-10-19 20:54:17 +0700

    feat: add icon for application

--- cmd/icon_generator.dart
+++ cmd/icon_generator.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 List<String> data = [];
 void main() async {
   // Đường dẫn đến thư mục bạn muốn lấy danh sách tên file
-  String folderPath = '../assets/icons/';
+  String folderPath = 'assets/icons/';
 
   // Gọi hàm lấy danh sách tên file
   List<String> fileNames = await getFileNamesInFolder(folderPath);
@@ -11,13 +11,12 @@ void main() async {
   // In danh sách tên file
   for (String fileName in fileNames) {
     final r = snakeCase(fileName.substring(0, fileName.length - 3));
-    data.add(
-        "static const String ic${r[0].toUpperCase()}${r.substring(1)} = 'assets/icons/$fileName';");
+    data.add("static const String ic${r[0].toUpperCase()}${r.substring(1)} = 'assets/icons/$fileName';");
   }
-  final file = File('../lib/core/constants/icon_resource.dart');
-  file.writeAsStringSync('abstract class IconRes { \n', mode: FileMode.write);
-  file.writeAsStringSync('\tconst IconRes._();\n',
-      mode: FileMode.writeOnlyAppend);
+  final file = File('lib/commons/constants/asset/icon_resource.dart');
+  file.writeAsStringSync('abstract class IconResource { \n', mode: FileMode.write);
+  file.writeAsStringSync('\tconst IconResource._();\n', mode: FileMode.writeOnlyAppend);
+  file.writeAsStringSync('\n', mode: FileMode.writeOnlyAppend);
   file.writeAsStringSync(data.join('\n\t'), mode: FileMode.writeOnlyAppend);
   file.writeAsStringSync('\n}', mode: FileMode.writeOnlyAppend);
 }
@@ -25,15 +24,11 @@ void main() async {
 Future<List<String>> getFileNamesInFolder(String folderPath) {
   Directory directory = Directory(folderPath);
   Stream<FileSystemEntity> entityList = directory.list();
-  return entityList
-      .where((entity) => entity is File)
-      .map((entity) => entity.path.split('/').last)
-      .toList();
+  return entityList.where((entity) => entity is File).map((entity) => entity.path.split('/').last).toList();
 }
 
 String snakeCase(String text) {
-  String a =
-      text.toLowerCase().replaceAllMapped(RegExp(r'[-_\s.]+(.)?'), (match) {
+  String a = text.toLowerCase().replaceAllMapped(RegExp(r'[-_\s.]+(.)?'), (match) {
     String? c = match.group(1);
     return c != null ? c.toUpperCase() : '';
   });
--- lib/commons/constants/asset/icon_resource.dart
+++ lib/commons/constants/asset/icon_resource.dart
@@ -1,3 +1,24 @@
 abstract class IconResource {
   const IconResource._();
+  static const String icArrowIosBackOutline = 'assets/icons/arrow-ios-back-outline.svg';
+  static const String icArrowIosDownwardOutline = 'assets/icons/arrow-ios-downward-outline.svg';
+  static const String icArrowIosForwardOutline = 'assets/icons/arrow-ios-forward-outline.svg';
+  static const String icArrowIosUpwardOutline = 'assets/icons/arrow-ios-upward-outline.svg';
+  static const String icBellOutline = 'assets/icons/bell-outline.svg';
+  static const String icBookmarkFill = 'assets/icons/Bookmark-fill.svg';
+  static const String icBookmarkLight = 'assets/icons/Bookmark-light.svg';
+  static const String icChatFill = 'assets/icons/Chat-fill.svg';
+  static const String icChatLight = 'assets/icons/Chat-light.svg';
+  static const String icEyeOffOutline = 'assets/icons/eye-off-outline.svg';
+  static const String icGlobeOutline = 'assets/icons/globe-outline.svg';
+  static const String icGridOutline = 'assets/icons/grid-outline.svg';
+  static const String icHeartOutline = 'assets/icons/heart-outline.svg';
+  static const String icHomeFill = 'assets/icons/Home-fill.svg';
+  static const String icHomeLight = 'assets/icons/Home-light.svg';
+  static const String icOptions2Outline = 'assets/icons/options-2-outline.svg';
+  static const String icPlusOutline = 'assets/icons/plus-outline.svg';
+  static const String icProfileFill = 'assets/icons/Profile-fill.svg';
+  static const String icProfileLight = 'assets/icons/Profile-light.svg';
+  static const String icSearchLight = 'assets/icons/Search-light.svg';
+  static const String icSearchOutline = 'assets/icons/search-outline.svg';
 }

a1baf26803d0599c93ed41f44a988561064402df
tungdevpro <tungdo21899@gmail.com>
2023-10-19 20:58:42 +0700

    hotfix* replace misspelled name

--- lib/commons/constants/app_constant.dart
+++ lib/commons/constants/app_constant.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 @immutable
-abstract class AppConsants {
-  const AppConsants._();
+abstract class AppConstants {
+  const AppConstants._();
 
   static const String title = "FiJob";
   static const String id = "iw90456";
   static const String domain = "http://localhost:3001/api";
   static const Locale startLocale = Locale("vi", "VN");
+  static const Iterable<Locale> supportedLocales = [Locale("vi", "VN"), Locale('en', 'US')];
 }

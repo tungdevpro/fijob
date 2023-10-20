534e223a3f388d8638ac25c813bab3ee5a514a79
tungdevpro <tungdo21899@gmail.com>
2023-10-21 06:54:10 +0700

    fix(app): change dependency injection

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -11,13 +11,17 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../core/network/dio_client_factory.dart' as _i4;
-import '../core/storage/hive_client_primitive_factory.dart' as _i5;
-import '../data/data_sources/remote/api_client_service.dart' as _i8;
-import '../data/repository_impls/auth_repository_impl.dart' as _i9;
-import '../presentation/auth/blocs/auth_bloc.dart' as _i3;
-import '../presentation/login/blocs/login_bloc.dart' as _i6;
-import '../presentation/splash/blocs/splash_cubit.dart' as _i7;
+import '../core/navigator/app_navigator.dart' as _i3;
+import '../core/network/dio_client_factory.dart' as _i5;
+import '../core/storage/hive_client_primitive_factory.dart' as _i7;
+import '../data/data_sources/remote/api_client_service.dart' as _i10;
+import '../data/repository_impls/auth_repository_impl.dart' as _i12;
+import '../domain/domain.dart' as _i11;
+import '../presentation/auth/blocs/auth_bloc.dart' as _i4;
+import '../presentation/getting_started/blocs/getting_started_cubit.dart'
+    as _i6;
+import '../presentation/login/blocs/login_bloc.dart' as _i8;
+import '../presentation/splash/blocs/splash_cubit.dart' as _i9;
 
 // initializes the registration of main-scope dependencies inside of GetIt
 _i1.GetIt initGetIt(
@@ -30,15 +34,17 @@ _i1.GetIt initGetIt(
     environment,
     environmentFilter,
   );
-  gh.singleton<_i3.AuthBloc>(_i3.AuthBloc());
-  gh.lazySingleton<_i4.DioClientFactory>(() => _i4.DioClientFactory());
-  gh.singleton<_i5.HiveClientPrimitiveFactory>(
-      _i5.HiveClientPrimitiveFactory());
-  gh.factory<_i6.LoginBloc>(() => _i6.LoginBloc());
-  gh.factory<_i7.SplashCubit>(() => _i7.SplashCubit());
-  gh.lazySingleton<_i8.ApiClientService>(
-      () => _i8.ApiClientService(gh<_i4.DioClientFactory>()));
-  gh.lazySingleton<_i9.AuthRepositoryImpl>(
-      () => _i9.AuthRepositoryImpl(gh<_i8.ApiClientService>()));
+  gh.lazySingleton<_i3.AppNavigator>(() => _i3.AppNavigator());
+  gh.singleton<_i4.AuthBloc>(_i4.AuthBloc());
+  gh.lazySingleton<_i5.DioClientFactory>(() => _i5.DioClientFactory());
+  gh.factory<_i6.GettingStartedCubit>(() => _i6.GettingStartedCubit());
+  gh.singleton<_i7.HiveClientPrimitiveFactory>(
+      _i7.HiveClientPrimitiveFactory());
+  gh.factory<_i8.LoginBloc>(() => _i8.LoginBloc());
+  gh.factory<_i9.SplashCubit>(() => _i9.SplashCubit());
+  gh.lazySingleton<_i10.ApiClientService>(
+      () => _i10.ApiClientService(gh<_i5.DioClientFactory>()));
+  gh.lazySingleton<_i11.AuthRepository>(
+      () => _i12.AuthRepositoryImpl(gh<_i10.ApiClientService>()));
   return getIt;
 }

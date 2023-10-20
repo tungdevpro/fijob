b3a12bfe9bb1f9db5a1b718dffb4642b830293de
tungdevpro <tungdo21899@gmail.com>
2023-10-21 06:46:48 +0700

    remove go_router package

--- lib/core/navigator/routes.dart
+++ lib/core/navigator/routes.dart
@@ -4,8 +4,8 @@ import 'package:fijob/presentation/getting_started/getting_started_page.dart';
 import 'package:fijob/presentation/splash/splash_page.dart';
 import 'package:fijob/presentation/login/login_page.dart';
 import 'package:fijob/presentation/register/register_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../presentation/home/home_page.dart';
 
@@ -13,27 +13,17 @@ import '../../presentation/home/home_page.dart';
 abstract class Routes {
   const Routes._();
 
-  static GoRouter router = GoRouter(
-    errorBuilder: (_, __) => const ErrorScreen(),
-    routes: [
-      GoRoute(path: RoutePath.initial, builder: (context, state) => const SplashPage()),
-      GoRoute(path: RoutePath.home, builder: (context, state) => const HomePage()),
-      GoRoute(path: RoutePath.gettingStarted, builder: (context, state) => const GettingStartedPage()),
-      GoRoute(path: RoutePath.login, builder: (context, state) => const LoginPage()),
-      GoRoute(path: RoutePath.register, builder: (context, state) => const RegisterPage())
-    ],
-  );
-}
+  static Map<String, WidgetBuilder> routes = {
+    RoutePath.initial: (_) => const SplashPage(),
+    RoutePath.home: (_) => const HomePage(),
+    RoutePath.gettingStarted: (_) => const GettingStartedPage(),
+    RoutePath.login: (_) => const LoginPage(),
+    RoutePath.register: (_) => const RegisterPage(),
+  };
 
-class CustomSlideTransition extends CustomTransitionPage<void> {
-  CustomSlideTransition({super.key, required super.child})
-      : super(
-          transitionDuration: const Duration(milliseconds: 300),
-          transitionsBuilder: (_, animation, __, child) {
-            return SlideTransition(
-              position: animation.drive(Tween(begin: const Offset(1.5, 0), end: Offset.zero).chain(CurveTween(curve: Curves.easeInOut))),
-              child: child,
-            );
-          },
-        );
+  static Route<dynamic> generateRoutes(RouteSettings settings) {
+    String? routeName = settings.name?.split('?').first;
+
+    return CupertinoPageRoute(builder: routes[routeName] ?? (_) => const Scaffold(body: ErrorScreen()), settings: settings);
+  }
 }

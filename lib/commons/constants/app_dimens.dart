c2a0bb706f9098078ad39cb24108c122a9bd4484
tungdevpro <tungdo21899@gmail.com>
2023-10-21 06:48:35 +0700

    fix(app): replace class name app_dimen to app_dimension

--- lib/commons/constants/app_dimens.dart
+++ lib/commons/constants/app_dimens.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 @immutable
-abstract class AppDimens {
-  const AppDimens._();
+abstract class AppDimension {
+  const AppDimension._();
 
+  static const double paddingSM = 10.0;
   static const double padding = 12.0;
+  static const double paddingLG = 16.0;
+  static const double paddingXXL = 24.0;
+  static const double maxPadding = 32.0;
   static const double radius = 24.0;
+  static const double radiusLG = 32.0;
 }

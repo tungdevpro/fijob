c27f5d64e3d694eb0fdb2cae9ee6812aa6257224
tungdevpro <tungdo21899@gmail.com>
2023-10-24 15:13:33 +0700

    feat(widget): add label in input field

--- lib/presentation/shared/custom_input_field.dart
+++ lib/presentation/shared/custom_input_field.dart
@@ -8,28 +8,38 @@ class CustomInputField extends StatelessWidget {
   final String? hintText;
   final ValueChanged<String>? onChanged;
   final String? errorText;
+  final dynamic labelText;
 
-  const CustomInputField({super.key, this.prefixIcon, this.hintText, this.suffixIcon, this.onChanged, this.errorText});
+  const CustomInputField({super.key, this.prefixIcon, this.hintText, this.suffixIcon, this.onChanged, this.errorText, this.labelText});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      cursorColor: ColorConstants.gray70,
-      cursorWidth: 1,
-      onChanged: (value) => onChanged?.call(value),
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(color: ColorConstants.gray70, fontSize: 14),
-        suffixIcon: Padding(padding: const EdgeInsets.all(13), child: suffixIcon),
-        prefixIcon: Padding(padding: const EdgeInsets.all(13), child: prefixIcon),
-        errorText: errorText,
-        border: OutlineInputBorder(
-            borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
-        enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
-        focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if(labelText != null) labelText is Widget ? labelText : Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(labelText, style: const TextStyle(fontWeight: FontWeight.w500, color: ColorConstants.gray100)),
+        ),
+        TextFormField(
+          cursorColor: ColorConstants.gray70,
+          cursorWidth: 1,
+          onChanged: (value) => onChanged?.call(value),
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(color: ColorConstants.gray70, fontSize: 16),
+            suffixIcon: Padding(padding: const EdgeInsets.all(13), child: suffixIcon),
+            prefixIcon: Padding(padding: const EdgeInsets.all(13), child: prefixIcon),
+            errorText: errorText,
+            border: OutlineInputBorder(
+                borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
+            enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(width: .5, color: ColorConstants.line), borderRadius: BorderRadius.circular(AppDimension.radius)),
+          ),
+        ),
+      ],
     );
   }
 }

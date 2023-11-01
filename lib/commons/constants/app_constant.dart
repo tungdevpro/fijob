9e44d7e6611bd5d5aa27af767e9207f3f43ad92b
tungdevpro <tungdo21899@gmail.com>
2023-11-01 20:20:16 +0700

    feat: add cache package for dio

--- lib/commons/constants/app_constant.dart
+++ lib/commons/constants/app_constant.dart
@@ -6,7 +6,7 @@ abstract class AppConstants {
 
   static const String title = "FiJob";
   static const String id = "iw90456";
-  static const String domain = "http://192.168.0.3:3002/v1";
+  static const String domain = "https://jsonplaceholder.typicode.com/";
   static const Locale startLocale = Locale("vi", "VN");
   static const Iterable<Locale> supportedLocales = [startLocale, Locale('en', 'US')];
 }

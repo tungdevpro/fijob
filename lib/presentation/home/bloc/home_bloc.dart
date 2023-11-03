41e0d667dbaaf26a76d8892846dac97ea3f4aa33
tungdevpro <tungdo21899@gmail.com>
2023-11-03 12:24:09 +0700

    fix(home): change event get post

--- lib/presentation/home/bloc/home_bloc.dart
+++ lib/presentation/home/bloc/home_bloc.dart
@@ -1,35 +1,32 @@
-import 'dart:async';
-
-import 'package:dartz/dartz.dart';
-import 'package:fijob/data/repository_impls/home_repository_impl.dart';
 import 'package:fijob/di/di.dart';
+import 'package:fijob/domain/repositories/home_repository.dart';
 import 'package:fijob/domain/usecases/home_get_new_job_usecase.dart';
 import 'package:fijob/presentation/home/bloc/home_event.dart';
-import 'package:fijob/presentation/home/bloc/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
-import 'package:yh_basic/common/blocs/base_bloc.dart';
-import 'package:yh_basic/common/blocs/base_state.dart';
+import 'package:yh_basic/common/blocs/blocs.dart';
+import 'package:yh_basic/yh_basic.dart';
 
-import '../../../data/data_source/remote/api_client_service.dart';
 import '../../../domain/enities/post_entity.dart';
 
 @injectable
-class HomeBloc extends BaseBloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeState(item: null));
+class HomeBloc extends BaseBloc<HomeEvent, ViewState<Post>> {
+  HomeBloc() : super(ViewState<Post>(data: null));
 
   static HomeBloc get to => getIt<HomeBloc>()..add(HomeGetPostEvent());
 
-  final homeRepo = HomeRepositoryImpl(getIt<ApiClientService>());
+  final homeRepo = getIt<HomeRepository>();
 
   @override
   void listEvent() {
     on<HomeGetPostEvent>(_onGetPost);
   }
 
-  void _onGetPost(HomeGetPostEvent event, Emitter<HomeState> emit) async {
+  void _onGetPost(HomeGetPostEvent event, Emitter<ViewState<Post>> emit) async {
+    emit(state.copyWith(status: ViewStateStatus.loading));
     final response = await HomeGetNewJobUseCase(homeRepo).execute();
-    response.fold((l) => null, (r) {
+    response.fold((l) => emit(state.copyWith(status: ViewStateStatus.error)), (r) async {
+      emit(state.copyWith(data: r.data, status: ViewStateStatus.success));
     });
   }
 }
--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,10 +1,9 @@
 import 'package:fijob/presentation/home/bloc/home_bloc.dart';
-import 'package:fijob/presentation/home/bloc/home_state.dart';
 import 'package:fijob/presentation/shared/app_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:yh_basic/shared/base_state_view.dart';
 import 'package:yh_basic/shared/shared.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,12 +22,12 @@ class _HomePageState extends BaseStateView<HomePage, HomeBloc> {
       value: bloc,
       child: Scaffold(
         appBar: AppHeader(text: "Home"),
-        body: ListView(
-          children: [
-            BlocBuilder<HomeBloc, HomeState>(
-              builder: (context, state) => Text('data...${state}'),
-            ),
-          ],
+        body: ViewStateBuilder<HomeBloc>(
+          onLoading: (context, data) => const CircularProgressIndicator(),
+          onError: (context, error) => Text('err: $error'),
+          onSuccess: (context, data) {
+            return Text('data...$data');
+          },
         ),
       ),
     );

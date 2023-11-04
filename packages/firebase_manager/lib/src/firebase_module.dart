0f5e0c24d3e3e1b983e32e4a62033e9e360776c0
tungdevpro <tungdo21899@gmail.com>
2023-11-04 14:51:07 +0700

    feat: add initialize & get token firebase

--- packages/firebase_manager/lib/src/firebase_module.dart
+++ packages/firebase_manager/lib/src/firebase_module.dart
@@ -1,5 +1,36 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+typedef OnInitialMsg = Function(RemoteMessage message);
+
 class FirebaseModule {
   static FirebaseModule? _instance;
 
   factory FirebaseModule() => _instance ??= FirebaseModule();
+
+  Future<void> initialize() async {
+    await Firebase.initializeApp();
+
+    await FirebaseMessaging.instance.requestPermission(alert: true, sound: true, criticalAlert: true);
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true, sound: true, badge: true);
+  }
+
+  Future<String> getToken() async {
+    try {
+      final result = await FirebaseMessaging.instance.getToken();
+      return result ?? '';
+    } catch (e) {
+      return '';
+    }
+  }
+
+  void listenForegroundNotification({VoidCallback? actionMessage}) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {});
+  }
+
+  void initialMessage({OnInitialMsg? onInitialMsg}) {
+    FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {});
+    }
 }

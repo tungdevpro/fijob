93a3df54b8f8608607fa3457fa8f1831a04d3368
tungdevpro <tungdo21899@gmail.com>
2023-11-08 15:00:07 +0700

    feat: add lang key

---
+++ lib/core/lang/lang_keys.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+@immutable
+abstract class LangKeys {
+  const LangKeys._();
+
+  static const String loginLabel = 'login_label';
+  static const String registerLabel = 'register_label';
+}

18b7fb6de2f6aef838f524dcc8550bb005b12878
tungdevpro <tungdo21899@gmail.com>
2023-11-14 15:06:37 +0700

    feat(lang): add vi/en language

--- lib/core/lang/lang_keys.dart
+++ lib/core/lang/lang_keys.dart
@@ -6,6 +6,20 @@ abstract class LangKeys {
 
   static const String loginLabel = 'login_label';
   static const String registerLabel = 'register_label';
+  static const String registerLabel2 = 'register_label_2';
+  static const String continueWith = 'continue_with';
+  static const String googleLabel = 'google_label';
+  static const String appleLabel = 'apple_label';
+  static const String emailField = 'email';
+  static const String emailFieldPlaceholder = 'email_placeholder';
+  static const String btnWithEmail = 'btn_email';
+  static const String hasAccount = 'has_account';
+  static const List<String> termsCondition = ['By signing up you agree to our', 'Terms' , 'and', 'Conditions of Use'];
+  static const String nextStepCompleteLabel = 'complete_label';
+  static const String fullNameField = 'fullname';
+  static const String fullNameFieldPlaceholder = 'fullname_placeholder';
+  static const String passwordField = 'password';
+  static const String passwordFieldPlaceholder = 'password_placeholder';
 
   // message error
   static const String errorCommon = 'error_common';

4520b26fad5bde8fa5652ecd6edd186ee8fa5827
tungdevpro <tungdo21899@gmail.com>
2023-11-17 10:15:11 +0700

    hotfix* replace google service plist file

--- lib/data/repository_impls/home_repository_impl.dart
+++ lib/data/repository_impls/home_repository_impl.dart
@@ -4,8 +4,7 @@ import 'package:fijob/data/data_source/remote/api_client_service.dart';
 import 'package:fijob/domain/enities/post_entity.dart';
 import 'package:fijob/domain/repositories/home_repository.dart';
 import 'package:injectable/injectable.dart';
-import 'package:yh_basic/common/api_response.dart';
-import 'package:yh_basic/common/app_error.dart';
+import 'package:yh_basic/yh_basic.dart';
 
 @LazySingleton(as: HomeRepository)
 class HomeRepositoryImpl implements HomeRepository {
@@ -16,8 +15,8 @@ class HomeRepositoryImpl implements HomeRepository {
   @override
   Future<Either<AppError, ApiResponse<List<Post>>>> getNewPost(PostRequester? params) async {
     try {
-      final response = await service.client
-          .get<List<Post>>(ApiPath.listPost, queryParameters: params?.toJson(), convertJson: (message) => message.map((e) => Post.fromJson(e)).toList().cast<Post>());
+      final response = await service.client.get<List<Post>>(ApiPath.listPost,
+          queryParameters: params?.toJson(), convertJson: (message) => message.map((e) => Post.fromJson(e)).toList().cast<Post>());
       return right<AppError, ApiResponse<List<Post>>>(response);
     } catch (e) {
       return left<AppError, ApiResponse<List<Post>>>(AppError(e: e));

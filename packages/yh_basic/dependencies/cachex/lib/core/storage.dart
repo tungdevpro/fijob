e28536abe5ceda39fcb8f8444b2f83757d746e1f
tungdevpro <tungdo21899@gmail.com>
2023-11-20 21:43:45 +0700

    feat(cache): add max age field

--- packages/yh_basic/dependencies/cachex/lib/core/storage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cachex/cachex.dart';
-
-class CacheManager {
-  final Configuration _configuration;
-
-  CacheManager(this._configuration) {
-    
-  }
-}
--- packages/yh_basic/dependencies/cachex/lib/models/data_local.dart
+++ packages/yh_basic/dependencies/cachex/lib/models/data_local.dart
@@ -2,13 +2,15 @@ class DataLocal {
   String? key;
   int? code;
   String? content;
+  int? maxAge;
 
-  DataLocal({this.key, this.code, this.content});
+  DataLocal({this.key, this.code, this.content, this.maxAge});
 
   DataLocal.fromJson(Map<String, dynamic> json) {
     key = json['key'];
     code = json['code'];
     content = json['content'];
+    maxAge = json['max_age'];
   }
 
   Map<String, dynamic> toJson() {
@@ -16,6 +18,7 @@ class DataLocal {
     data['key'] = key;
     data['code'] = code;
     data['content'] = content;
+    data['max_age'] = maxAge;
     return data;
   }
 }
